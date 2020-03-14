2a1ccf9e403b721fae6fe8a70d703df4dfc59e91
qing <17600969308@163.com>
2020-03-14 17:25:12 +0800

    com

--- application/admin/admin/Company.php
+++ application/admin/admin/Company.php
@@ -19,6 +19,7 @@ class Company extends Admin
 
 //            echo $keyword;
             $where[]    = ['1' , 'eq' , 1];
+            $where[] = ['id', '=', 1];
 
             $data['data'] = Db::table('c_company')->where($where)->page($page)->limit($limit)->order('c_time' , 'desc')->select();
 
@@ -108,6 +109,111 @@ class Company extends Admin
      * 下面是单条件删除
      */
     public function del(){
+
+        if ($this->request->isPost()){
+            $ids = $this->request->post('id');
+
+            $id_str = implode(',' , $ids);
+            $res = Db::table('c_company')->where('id' , 'in' , $id_str)->delete();
+            if (!$res){
+                return $this->error('删除失败');
+            }
+
+            return $this->success("删除成功");
+        }
+        $id = get_num();
+
+        $res = Db::table('c_company')->where('id' , $id)->delete();
+        if (!$res){
+            return $this->error('删除失败');
+        }
+
+        return $this->success('删除成功');
+    }
+
+
+
+    public function telindex(){
+        if ($this->request->isAjax()) {
+            $where      = $data = [];
+            $page       = $this->request->param('page/d', 1);
+            $limit      = $this->request->param('limit/d' , 200);
+
+//            echo $keyword;
+            $where[]    = ['1' , 'eq' , 1];
+            $where[] = ['id', '=', 2];
+
+            $data['data'] = Db::table('c_company')->where($where)->page($page)->limit($limit)->order('c_time' , 'desc')->select();
+
+            $data['count'] = Db::table('c_company')->where($where)->count('id');
+            $data['code'] = 0;
+            $data['msg'] = '';
+            return json($data);
+        }
+
+        return $this->fetch();
+    }
+
+
+    /**
+     * 添加操作
+     * isPost内执行添加操作
+     * 下面是显示添加页面
+     */
+    public function teladd(){
+        if ($this->request->isPost()) {
+            $data = $this->request->post();
+
+            $data['c_time'] = time();
+
+            $res = Db::table("c_company")->insert($data);
+
+            if (!$res) {
+                return $this->error('添加失败');
+            }
+            return $this->success('添加成功');
+        }
+
+        return $this->fetch();
+    }
+
+    /**
+     * 编辑操作
+     * isPost内执行修改操作
+     * get_num()调取公共方法，默认取值为id如有改变参数请填写
+     * 下面是显示修改页面
+     */
+    public function teledit(){
+        if ($this->request->isPost()) {
+            $data = $this->request->post();
+
+//            $data['image'] = str_replace('/warehouse/hisiphp/public' , '' , $data['image']);
+            $data['c_time'] = time();
+
+            $res = Db::table("c_company")->update($data);
+
+            if (!$res) {
+                return $this->error('修改失败');
+            }
+            return $this->success('修改成功');
+        }
+
+        $id = get_num();
+        $info = Db::table("c_company")->where('id' , $id)->find();
+
+
+        $this->assign('info' , $info);
+
+        return $this->fetch();
+    }
+
+    /* *
+     * 删除方法
+     * isPost内是多条件删除
+     * get_num()调取公共方法，默认取值为id如有改变参数请填写
+     * 下面是单条件删除
+     */
+    public function teldel(){
 
         if ($this->request->isPost()){
             $ids = $this->request->post('id');
